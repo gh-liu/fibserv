c93e6b06f8e80da9ef20ea01abed8f71328f27ef
gh-liu <liugh.cs@gmail.com>
2023-11-08 17:35:31 +0800

    init3

--- libs/helpers.c
+++ libs/helpers.c
@@ -1,6 +1,8 @@
 #include "structs.h"
 #include <stdlib.h>
 #include <string.h>
+#include <uriparser/Uri.h>
+#include <uriparser/UriBase.h>
 
 void wrapStrFromPTR(char *str, size_t len, const char *head, const char *tail) {
   for (size_t i = 0; head != tail; head++)
@@ -31,4 +33,56 @@ void setupServerSettings(int argc, const char **argv, serverSettings *ss) {
       ss->threadCount = atoi(val);
     }
   }
+}
+
+int retrieveGETQueryIntValByKey(char *req, const char *key) {
+  int result = 0;
+
+  const char *uriHead = strchr(req, ' ') + 1;
+  const char *uriTail = strchr(uriHead, ' ');
+  size_t uriLen = uriTail - uriHead + 1;
+  char strUri[uriLen];
+  wrapStrFromPTR(strUri, uriLen, uriHead, uriTail);
+
+  UriUriA uri;
+  const char *errorPos;
+  if (uriParseSingleUriA(&uri, strUri, &errorPos) == URI_SUCCESS) {
+    UriQueryListA *querylist;
+    int itemCount;
+    if (uriDissectQueryMallocA(&querylist, &itemCount, uri.query.first,
+                               uri.query.afterLast) == URI_SUCCESS) {
+      while (itemCount--) {
+        if (strcmp(querylist->key, key) == 0) {
+          result = atoi(querylist->value);
+          break;
+        }
+        querylist = querylist->next;
+      }
+      uriFreeQueryListA(querylist);
+    }
+  }
+  return result;
+}
+
+// tail call optimization
+int __calcFibTCO(int n, int x, int y) {
+  if (n == 0) {
+    return x;
+  }
+  if (n == 1) {
+    return y;
+  }
+  return __calcFibTCO(n - 1, y, y + x);
+}
+
+int __calcFibRecusrion(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  return __calcFibRecusrion(n - 1) + __calcFibRecusrion(n - 2);
+}
+
+int calcFibonacci(int n) {
+  // return __calcFibTOC(n, 0, 1);
+  return __calcFibRecusrion(n);
 }
